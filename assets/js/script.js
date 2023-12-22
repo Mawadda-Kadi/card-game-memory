20fe59955a9ce29dfd2110aa1334a87171524a39
Mawadda-Kadi <>
2023-12-22 07:09:46 +0000

    add: basic functions names to JS

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+/* Function */
+function difficulityLevel() {
+
+}
+
+function startGame() {
+
+}
+
+function shuffleCards() {
+
+}
+
+function startTimer() {
+
+}
+
+function updateTimer() {
+
+
+}
+
+function selectCard() {
+
+}
+
+function filpCard() {
+
+}
+
+function checkForMatch() {
+
+}
+
+function updateNumberOfMatchedCards() {
+
+
+}
+
+function updateMoves() {
+
+}
+
+function gameWon() {
+
+}
+
+function displayGameOverModal() {
+
+
+}
+
+function pauseOrResume() {
+
+
+}
+
+function restGame() {
+
+
+}
+
+
